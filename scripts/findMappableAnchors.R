5f54137630e48ef8e81a7a5e6b0b07d55bb059bb
nicmoya <nicolasmoya2024@u.northwestern.edu>
2026-01-26 19:26:28 -0500

    Adding first attempts at exploring logic for finding mappable anchors.

--- scripts/findMappableAnchors.R
+++ scripts/findMappableAnchors.R
@@ -41,13 +41,115 @@ orthos <- orthos %>% dplyr::select(-AF16.WBPS19,-JU1422.WBPS19) #other reference
 
 #list of nigoni strains included in orthofinder
 #mainly included for exploratory purposes, they are later omitted from the plots due to lack of homology in chromosomal arms between both C.b. and C.n.
-#This will change in the future, once t
+#This will change in the future, once the mapping and anchoring logic and sample selection is refined
 nigonis <- c("JU1422","ECA2852","ECA2857","EG5268","JU1418","JU2617","JU1419","JU2484","JU4356","NIC2143","NIC2150","NIC2152","VSL2202","VX153","YR106","ZF1220")
 #refs <- c("MY681","JU3207","JU2536")
 
+refrence_bins <- readr::read_tsv("../working_data/QX1410_genomic_windows.1kb.bed",col_names = c("CHROM","START","END"))
+
 #instead of targeting a region at the alignment stage, we will construct a map of anchors across a defined set of samples. 
 #These anchors can then be queried for visualization.
+#first, some exploratory work
 
+#high confidence intervals
+#we drop the nigonis for now
+transformed_coords_hidy <- transformed_coords %>% 
+  dplyr::filter(IDY>=95) %>%
+  dplyr::filter(L1 >1e4 & L2 > 1e4) %>%
+  dplyr::filter(!(STRAIN %in% nigonis))
 
+#intervals to data table
+dt <- as.data.table(transformed_coords_hidy)
+dt[, CHROM := REF]
 
+#we don't assume orientation of reference contig
+dt[, `:=`(
+  ref_start = pmin(S1, E1),
+  ref_end   = pmax(S1, E1)
+)]
+
+# keep S2/E2 and HIFI so they survive into ov
+dt <- dt[, .(CHROM, STRAIN, IDY, ref_start, ref_end, HIFI, S2, E2)]
+
+#genomic bins to data table
+bins <- as.data.table(refrence_bins)
+setnames(bins, c("START", "END"), c("bin_start", "bin_end"))
+
+#set keys for overlap 
+setkey(bins, CHROM, bin_start, bin_end)
+setkey(dt, CHROM, ref_start, ref_end)
+
+#find overlaps
+ov <- foverlaps(
+  x = dt,
+  y = bins,
+  by.x = c("CHROM", "ref_start", "ref_end"),
+  by.y = c("CHROM", "bin_start", "bin_end"),
+  type = "any",
+  nomatch = 0L
+)
+
+#set midpoint
+ov[, bin_mid := (bin_start + bin_end) / 2]
+
+# alignment length on the WI genome 
+ov[, aln_len := abs(E2 - S2)]
+
+# sort so the best candidate is first within each bin
+setorder(ov, CHROM, STRAIN, bin_start, -aln_len,-IDY)
+
+# keep one row per bin (drops remaining ties as duplicates)
+#we then explore data at the alignment level (rather than bin level)
+hm <- ov[, .SD[1], by = .(CHROM, STRAIN, bin_start, bin_end, bin_mid)] %>%
+  dplyr::select(CHROM,STRAIN,IDY,ref_start,ref_end,HIFI,S2,E2,aln_len) %>%
+  dplyr::group_by(STRAIN,CHROM)%>%
+  dplyr::distinct(HIFI,S2,.keep_all = T)
+
+#df for ggplot
+df <- as.data.frame(hm) 
+
+# order strains (optional but usually nicer)
+strain_levels <- df %>%
+  group_by(STRAIN) %>%
+  summarise(mean_idy = mean(IDY, na.rm = TRUE), .groups = "drop") %>%
+  arrange(desc(mean_idy)) %>%
+  pull(STRAIN)
+
+df <- df %>%
+  mutate(
+    STRAIN = factor(STRAIN, levels = strain_levels),
+    y = as.numeric(STRAIN)
+  )
+
+#genome accessibility map
+ggplot(df) +
+  geom_rect(aes(
+    xmin = ref_start,
+    xmax = ref_end,
+    ymin = y - 0.45,
+    ymax = y + 0.45,
+    fill = HIFI
+  )) +
+  facet_grid(CHROM ~ ., scales = "free_x", space = "free_x") +
+  # scale_fill_gradientn(
+  #   colours = c("red", "yellow", "green"),
+  #   limits = c(96, 100),
+  #   oob = scales::squish,
+  #   name = "IDY"
+  # ) +
+  scale_x_continuous(
+    labels = label_number(scale = 1e-6, suffix = " Mb")
+  ) +
+  scale_y_continuous(
+    breaks = seq_along(levels(df$STRAIN)),
+    labels = levels(df$STRAIN),
+    expand = c(0.01, 0.01)
+  ) +
+  labs(x = "Reference position", y = "STRAIN") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),legend.position = "none")
+
+#other diagnostic plots
+ggplot() + geom_segment(data=hm %>%dplyr::filter(CHROM=="I"),aes(y=E2 ,yend=S2,x=ref_start,xend=ref_end,color=HIFI))
+ggplot() + geom_segment(data=test,aes(y=S2/1e6 ,yend=E2/1e6,x=ref_start/1e6,xend=ref_end/1e6,color=HIFI)) +xlim(0,5)
 
